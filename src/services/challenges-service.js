f100fc7d5ad30ba016a7676ca3512340e98dc644
Esther Bernal <esther.bernal.lopez@gmail.com>
2019-08-14 18:09:30 +0200

    challenge services

---
+++ src/services/challenges-service.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+class ChallengeService {
+  constructor(){
+    this.challenge = axios.create({
+      baseURL: 'http://localhost:4000/challenges'
+    });
+  }
+
+  getAllChallenges(){
+    return this.appStore.get('/')
+    .then(response => response) 
+  };
+
+  getOneChallenge(id){
+    return this.appStore.get(`/${id}`)
+    .then(response => response) 
+  };
+
+  addOneChallenge(newChallenge){
+    return this.appStore.post('/add', newChallenge)
+    .then(response => response)
+
+  }
+  
+  updtateOneChallenge(id, updateChallenge){
+    return this.appStore.put(`/${id}/edit`,updateChallenge)
+    .then(response => response);
+  }
+
+  deleteOneChallenge(id){
+    return this.appStore.delete(`/${id}/delete`)
+    .then(response => response);
+  }
+}
+
+const challengeService = new ChallengeService();
+
+export default challengeService;
